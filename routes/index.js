46063aa322f76ac6b0d557687eba1abd132d7cdc
programmnik <nikitasmatko184@gmail.com>
2026-01-05 02:35:28 +0300

    Set project. Mongodb with prisma. Controllers

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const { UserController } = require('../controllers');
+const { PostController } = require('../controllers');
+const authenticateToken = require('../middleware/auth');
+
+const uploadDestination = 'uploads';
+
+//Место для хранения файлов
+const storage = multer.diskStorage({
+    destination: uploadDestination,
+    filename: function(req, file, cb){
+        cb(null, file.originalname);
+    }
+});
+
+const uploads = multer({storage: storage})
+
+//Роуты для пользователя
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+router.get('/current', authenticateToken, UserController.current);
+router.get('/users/:id', authenticateToken, UserController.getUserById);
+router.put('/users/:id', authenticateToken, UserController.updateUser);
+
+//Роуты для постов
+router.post('/posts', authenticateToken, PostController.createPost);
+router.get('/posts', authenticateToken, PostController.getAllPosts);
+router.get('/posts/:id', authenticateToken, PostController.getPostById);
+router.delete('/posts/:id', authenticateToken, PostController.deletePost);
+
+
+module.exports = router;
