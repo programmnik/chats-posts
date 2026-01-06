7287ce0f892e31cbfd05a052b9f61b74ec573971
programmnik <nikitasmatko184@gmail.com>
2026-01-06 20:42:38 +0300

    follow-controller

--- controllers/follow-controller.js
+++ controllers/follow-controller.js
@@ -1,7 +1,71 @@
 const { prisma } = require("../prisma/prisma-client");
 
 const FollowController = {
+    followUser: async (req, res) =>{
+        const {followingId} = req.body;
+        const userId = req.user.userId;
 
+        if(followingId === userId){
+            return res.status(500).json({error: "Вы не можете подписаться на самого себя"});
+        }
+
+        try{
+            const existingSubscription = await prisma.follows.findFirst({
+                where: {
+                    AND: [
+                        {followerId: userId},
+                        {followingId}
+                    ]
+                }
+            });
+
+            if(existingSubscription){
+                return res.status(400).json({error: "Подписка уже существует"});
+            }
+
+            await prisma.follows.create({
+                data: {
+                    follower: {connect: {id: userId}},
+                    following: {connect: {id: followingId}}
+                }
+            })
+
+            res.status(201).json({message: 'Подписка успешно создана'});
+        }
+        catch(error){
+            console.log('Follow error ', error);
+            return res.status(500).json({error: 'Internal server error'});
+        }
+    },
+    unFollowUser: async (req, res) =>{
+        const {followingId} = req.body;
+        const userId = req.user.userId;
+
+        try{
+            const follows = await prisma.follows.findFirst({
+                where: {
+                    AND:[
+                        {followerId: userId},
+                        {followingId}
+                    ]
+                }
+            });
+
+            if(!follows){
+                return res.status(404).json({error: 'Нет подписки на данного пользователя'});
+            }
+
+            await prisma.follows.delete({
+                where: { id: follows.id }
+            })
+
+            res.status(201).json({message: 'Вы отписались'});
+        }
+        catch(error){
+            console.log('Unfollow error ', error);
+            return res.status(500).json({error: 'Internal server error'});
+        }
+    }
 }
 
 module.exports = FollowController;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { UserController, PostController, CommentController, LikeController } = require('../controllers');
+const { UserController, PostController, CommentController, LikeController, FollowController } = require('../controllers');
 const authenticateToken = require('../middleware/auth');
 
 const uploadDestination = 'uploads';
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
     }
 });
 
-const uploads = multer({storage: storage})
+const uploads = multer({storage: storage});
 
 //Роуты для пользователя
 router.post('/register', UserController.register);
@@ -36,5 +36,9 @@ router.delete('/comments/:id', authenticateToken, CommentController.deleteCommen
 //Роуты для лайков
 router.post('/likes', authenticateToken, LikeController.likePost);
 router.delete('/likes/:id', authenticateToken, LikeController.unlikePost);
+
+//Роуты для подписок
+router.post('/follow', authenticateToken, FollowController.followUser);
+router.delete('/unfollow/:id', authenticateToken, FollowController.unFollowUser);
 
 module.exports = router;
